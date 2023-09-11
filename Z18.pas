81b9721c156bf06495623f26e46a23ba57880c79
Kotobulka <144238123+Kotobulka@users.noreply.github.com>
2023-09-11 10:54:30 +0300

    Add files via upload

---
+++ Z18.pas
@@ -0,0 +1,8 @@
+﻿Program Z18;
+var a: integer;
+begin
+  writeln('Введите год');
+  read(a);
+  If a mod 4 = 0 then writeln('Год високосный');
+  if a mo 4 <> 0 then writeln('Год не високосный');
+end.
