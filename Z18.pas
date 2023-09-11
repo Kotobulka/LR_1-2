d77faae85258758976c232e6ef80de2eb36cffd5
Kotobulka <144238123+Kotobulka@users.noreply.github.com>
2023-09-11 11:08:27 +0300

    Update Z18.pas

--- Z18.pas
+++ Z18.pas
@@ -1,8 +1,10 @@
-﻿Program Z18;
+Program Z18;
 var a: integer;
 begin
   writeln('Введите год');
   read(a);
-  If a mod 4 = 0 then writeln('Год високосный');
-  if a mo 4 <> 0 then writeln('Год не високосный');
+  If a mod 4 = 0 then
+    writeln('Год високосный')
+ else
+       writeln ('Год не висококсный');
 end.
